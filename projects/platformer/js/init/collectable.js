f97f48c407b40e9c3a4720755eb6cd6bf2a05705
GЯIM <aebouzigard@gmail.com>
2021-12-02 18:43:44 +0000

    collectables

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -38,8 +38,8 @@
         
         // example: 
         createCollectable(type.steve, 200, 170, 6, 0.7);
-        
-        
+        createCollectable(type.grace, 600, 470, 6, 0.7);
+        createCollectable(type.db, 500, 170, 6, 0.7);
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
