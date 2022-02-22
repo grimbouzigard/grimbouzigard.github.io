ecd8176dfbdbe430439f7efdc1731b85fba77025
GЯIM <aebouzigard@gmail.com>
2022-02-22 15:13:13 +0000

    todo 1 - 3

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -36,11 +36,17 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'black');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
-            
+            var moon = draw.bitmap('img/moon.png'); //created a variable called moon. draw.bitmap stores it in the variable
+            moon.x = 800; //moves the moon left and right
+            moon.y = 200; //moves the moon up and down
+            moon.scaleX = 0.5; //scales the moon stretches the moon horizontal
+            moon.scaleY = 0.5; //scales the moon stretches the moon vertical
+            background.addChild(moon); //lets us see the moon on the screne
+
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
