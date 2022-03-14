131f4fa8b048af224b7fd129989c3a5c0a6c778a
GЯIM <aebouzigard@gmail.com>
2022-03-14 14:13:08 +0000

    enemy

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -40,15 +40,46 @@ var level01 = function (window) {
             sawBladeHitZone.addChild(obstacleImage); //add image to the hitzone so we can see it
             obstacleImage.x = -25; //tweaks the image -25 pixels left
             obstacleImage.y = -25; //tweaks the image -25 pixels left
+            sawBladeHitZone.rotationalVelocity = 25;
         }
 
         createSawBlade(400, 345);
         createSawBlade(600, 345);
         createSawBlade(800, 345);
+       
+        function createEnemy(x, y){
+        var enemy = game.createGameItem('enemy',25); //creating the game item and storing it in the variable enemy
+        var redSquare = draw.rect(50,50,'red'); //creates rectangle and stores as red square
+        redSquare.x = -25;
+        redSquare.y = -25;
+        enemy.addChild(redSquare); //add the red square to the enemy game item
+
+        enemy.x = 400;
+        enemy.y = groundY-50;
+
+        game.addGameItem(enemy); //adds enemy to the game
+        enemy.velocityX = -1; // causes the enemy to move 1 pixel to the left
         
-        // DO NOT EDIT CODE BELOW HERE
+        enemy.rotationalVelocity = 25;
+
+        enemy.onPlayerCollision = function() {
+            console.log('The enemy has hit Halle');
+            game.changeIntegrity(-10);
+        };
+        
+        enemy.onProjectileCollision = function() {
+            console.log('The enemy has hit Halle');
+            game.changeIntegrity(5);
+            game.increaseScore(10);
+            enemy.fadeOut();
+        };
     }
-};
+    createEnemy();
+    createEnemy();
+    createEnemy(20, 20);
+        // DO NOT EDIT CODE BELOW HERE
+}
+    };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
